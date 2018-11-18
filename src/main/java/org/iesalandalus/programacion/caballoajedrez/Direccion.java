0d575043b62916c680f0aec2222d8bcc9a8c6ae2
JFQueroDev <JFQueroDev@192.168.1.136>
2018-11-18 13:22:31 +0100

    Creado el enumerado Direccion, descomentado y pasado el test pertinente.

---
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Direccion.java
@@ -0,0 +1,5 @@
+package org.iesalandalus.programacion.caballoajedrez;
+
+public enum Direccion {
+	ARRIBA_IZQUIERDA, ARRIBA_DERECHA, DERECHA_ARRIBA, DERECHA_ABAJO, ABAJO_IZQUIERDA, ABAJO_DERECHA, IZQUIERDA_ARRIBA, IZQUIERDA_ABAJO;
+}
