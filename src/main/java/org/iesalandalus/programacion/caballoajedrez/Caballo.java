2aa0d02049238a3cbfa7515e36e464958979950a
JFQueroDev <JFQueroDev@192.168.1.136>
2018-11-18 19:36:36 +0100

    Creados los metodos get y set para Caballo

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
@@ -1,8 +1,25 @@
 package org.iesalandalus.programacion.caballoajedrez;
 
 public class Caballo {
-	
+
 	private Color color;
 	private Posicion posicion;
 
+	/* Metodos para color */
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
+
+	/* Metodos para posicion */
+	public Posicion getPosicion() {
+		return posicion;
+	}
+	
+	public void setPosicion(Posicion posicion) {
+		this.posicion = posicion;
+	}
 }
